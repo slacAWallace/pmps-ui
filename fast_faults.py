3fc968934fcda97ba6295c260b158be489230164
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-06-29 15:31:31 -0700

    WIP: Hide disconnected panels.

--- fast_faults.py
+++ fast_faults.py
@@ -1,18 +1,40 @@
 import json
+import functools
 import itertools
 from string import Template
 from qtpy import QtWidgets, QtCore
 from pydm import Display
 from pydm.widgets import PyDMEmbeddedDisplay
+from pydm.widgets.channel import PyDMChannel
+
+
+def clear_channel(ch):
+    if ch:
+        try:
+            ch.disconnect()
+        except:
+            pass
 
 
 class VisibilityEmbedded(PyDMEmbeddedDisplay):
+
+    def __init__(self, channel=None, *args, **kwargs):
+        super(VisibilityEmbedded, self).__init__(*args, **kwargs)
+        self.setVisible(False)
+        self._connected = None
+        self.channel = None
+        if channel:
+            self.channel = PyDMChannel(channel, connection_slot=self.connection_changed)
+        self.destroyed.connect(functools.partial(clear_channel, channel))
+
+    def connection_changed(self, status):
+        self._connected = status
 
     @QtCore.Slot(list)
     def update_filter(self, filters):
         self.rules = "[]"
 
-        if not filters:
+        if not filters and self._connected:
             self.setVisible(True)
             return
         channels = []
@@ -71,7 +93,8 @@ class FastFaults(Display):
                 s_ffo = str(_ffo).zfill(ffos_zfill)
                 s_ff = str(_ff).zfill(ffs_zfill)
                 macros = dict(index=count, P=prefix, FFO=s_ffo, FF=s_ff)
-                widget = VisibilityEmbedded(parent=ff_container)
+                ch = Template('ca://${P}FFO:${FFO}:FF:${FF}:Info:InUse_RBV').safe_substitute(**macros)
+                widget = VisibilityEmbedded(parent=ff_container, channel=ch)
                 widget.prefixes = macros
                 self.filters_changed[list].connect(widget.update_filter)
                 widget.macros = json.dumps(macros)
@@ -79,11 +102,11 @@ class FastFaults(Display):
                 widget.disconnectWhenHidden = False
                 ff_container.layout().addWidget(widget)
                 count += 1
-
         vertical_spacer = QtWidgets.QSpacerItem(20, 40,
-                                               QtWidgets.QSizePolicy.Minimum,
-                                               QtWidgets.QSizePolicy.Expanding)
+                                               QtWidgets.QSizePolicy.Preferred,
+                                               QtWidgets.QSizePolicy.MinimumExpanding)
         ff_container.layout().addItem(vertical_spacer)
+        self.update_filters()
         print(f'Added {count} fast faults')
 
     def ui_filename(self):
