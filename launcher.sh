c0254d968932a93c76ff264798eb3a9da1525f53
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-06-29 18:04:23 -0700

    FIX: Fixing launcher so it can be used from other folders.

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source /reg/g/pcds/pyps/conda/dev_conda
+pushd /reg/neh/home/slepicka/sandbox/git-pcdshub/pmps-ui
 PYTHONPATH=/reg/neh/home/slepicka/sandbox/git-slaclab/pydm-git:$PYTHONPATH pydm --hide-nav-bar --hide-menu-bar --hide-status-bar pmps.py
+popd
